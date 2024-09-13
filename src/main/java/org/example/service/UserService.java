cfca0637f9bd1409fc80bd95c78f285497e8abc8
HissDev <hissoffc@gmail.com>
2024-09-13 23:20:22 +0500

    Жизненный цикл тестов. @BeforeAll -> @BeforeEach -> @Test -> @AfterEach -> @AfterAll * @BeforeAll - вызывается перед всеми тестами. Далее происходит череда вызовов без учёта порядка тестов! * Происходит вызов @BeforeEach и @AfterEach * Вызов всех тестов * Вызов @AfterAll

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,13 +2,19 @@ package org.example.service;
 
 import org.example.dto.User;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class UserService {
 
+    private final List<User> users = new ArrayList<>();
+
     public List<User> getAll() {
-        return Collections.emptyList();
+        return users;
     }
 
+    public boolean add(User user) {
+        return users.add(user);
+    }
 }
--- src/test/java/org/example/service/UserServiceTest.java
+++ src/test/java/org/example/service/UserServiceTest.java
@@ -2,23 +2,59 @@ package org.example.service;
 
 
 import org.example.dto.User;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    void init() {
+        System.out.println("Before all");
+    }
+
+    @BeforeEach
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
+    }
 
     @Test
 //    Проверяем пустая ли коллекция пользователей
     void usersEmptyIfNoUserAdded() {
+        System.out.println("Test 1: " + this);
 
-        UserService userService = new UserService();
         List<User> users = userService.getAll();
 
         // Проверяет результат который возвращает true
         assertTrue(users.isEmpty(), () -> "User list is empty");
+    }
+
+    @Test
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
+
+        userService.add(new User());
+        userService.add(new User());
+
+        List<User> users = userService.getAll();
+        assertEquals(2, users.size());
+    }
+
+    @AfterEach
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
+    }
+
+    @AfterAll
+    void closeConnectionPool() {
+        System.out.println("After all");
     }
 
 }
