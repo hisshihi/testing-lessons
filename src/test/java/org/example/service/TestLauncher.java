1f713f811a0f4463fccb2d39343915db01b1e5f8
HissDev <hissoffc@gmail.com>
2024-09-15 16:26:58 +0500

    Добавил библиотеку и класс для запуска тестов, для демонстрации того, как среды разработки делают это.

---
+++ src/test/java/org/example/service/TestLauncher.java
@@ -0,0 +1,36 @@
+package org.example.service;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+// Собственный класс для запуска тестов
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        // Создания экземпляря launcher для запуска тестов
+        Launcher launcher = LauncherFactory.create();
+        // Создание слушателя для генерации сводки результатов тестирования
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+        // Создание запросов на обнаружение тестов
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+                // Поиск всех тестов в указанном объекте
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                // Поиск всех тестов в указанном пакете
+                .selectors(DiscoverySelectors.selectPackage("org.example.service"))
+                .build();
+        // Выполнение тестов с использованием заданного запроса и слушателя
+        launcher.execute(request, summaryGeneratingListener);
+        // Печать результатов в консоль
+        try (PrintWriter writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+
+}
